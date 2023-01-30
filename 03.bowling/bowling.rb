5f8c9e939eebc5cc4d305291628c671e671943b3
nekonote <nekonote.civ@gmail.com>
2023-01-30 11:49:09 +0000

    rubocop の警告に合わせて変数名や記述方法を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,7 +4,7 @@
 MAX_FRAMES = 10
 
 # 特殊スコア
-POINT_SPARE  = 10 # スペア
+POINT_SPARE = 10 # スペア
 POINT_STRIKE = 10 # ストライク
 
 # 最終フレームかどうか
@@ -13,27 +13,27 @@ def last_frame?(now_frame)
 end
 
 # ストライクかどうか
-def strike?(num_scores, cursor)
-  num_scores[cursor] == POINT_STRIKE
+def strike?(scores, cursor)
+  scores[cursor] == POINT_STRIKE
 end
 
 # スペアかどうか
-def spare?(num_scores, cursor)
-  !strike?(num_scores, cursor) && num_scores[cursor] + num_scores[cursor + 1] == POINT_SPARE
+def spare?(scores, cursor)
+  !strike?(scores, cursor) && scores[cursor] + scores[cursor + 1] == POINT_SPARE
 end
 
 # スコアを引数から取得して配列化
-score = ARGV[0]
-scores = score.split(',')
+argv = ARGV[0]
+argv_splits = argv.split(',')
 
 # スコアを数値化
-num_scores = scores.map { |score|
+scores = argv_splits.map do |score|
   if score == 'X'
-    POINT_STRIKE 
+    POINT_STRIKE
   else
     score.to_i
   end
-}
+end
 
 point     = 0 # スコアの合計値
 cursor    = 0 # 配列内の現在地
@@ -42,20 +42,18 @@ now_frame = 0 # 現在のフレーム数
 while now_frame <= MAX_FRAMES - 1
 
   if last_frame?(now_frame)
-    point += num_scores.
-      slice(cursor, num_scores.size - cursor)
-      .sum
+    point += scores.slice(cursor, scores.size - cursor).sum
     break
   end
 
-  if spare?(num_scores, cursor)
-    point += (POINT_SPARE + num_scores[cursor + 2])
+  if spare?(scores, cursor)
+    point += (POINT_SPARE + scores[cursor + 2])
     cursor += 2 # カーソル位置更新
-  elsif strike?(num_scores, cursor)
-    point += (POINT_STRIKE + num_scores[cursor + 1] + num_scores[cursor + 2])
+  elsif strike?(scores, cursor)
+    point += (POINT_STRIKE + scores[cursor + 1] + scores[cursor + 2])
     cursor += 1 # カーソル位置更新
   else
-    point += (num_scores[cursor] + num_scores[cursor + 1])
+    point += (scores[cursor] + scores[cursor + 1])
     cursor += 2 # カーソル位置更新
   end
 
