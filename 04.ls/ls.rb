f8a37837e782159c583aad3af9f3d6ba8f615a48
nekonote <nekonote.civ@gmail.com>
2023-02-24 13:25:55 +0000

    必要なメソッドを切り出してrubocopの行数チェックに対応した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -9,6 +9,7 @@ NUMBER_OF_COLUMNS = 3
 SINGLE_BYTE_CHAR_DISPLAY_LENGTH = 1
 MULTI_BYTE_CHAR_DISPLAY_LENGTH = 2
 OFFSET_SPACES = '  '
+DEVICE_DISPLAY_OFFSET = 2
 
 def translate_display_char_length(char)
   char.bytesize == 1 ? SINGLE_BYTE_CHAR_DISPLAY_LENGTH : MULTI_BYTE_CHAR_DISPLAY_LENGTH
@@ -100,9 +101,125 @@ def convert_special_permission(octal_mode, permission)
 end
 
 def convert_permission(mode)
+  # 8進数に変換(扱いやすいように0パティング)
   octal_mode = mode.to_s(8).rjust(6, '0')
   permission = convert_access_permission(octal_mode)
   convert_special_permission(octal_mode, permission)
+end
+
+# オプションにディレクトリが指定された場合、末尾に "/" を付与して返却
+def base_path(argv)
+  if !argv.empty? && FileTest.directory?(argv)
+    base_path = argv[-1] != '/' ? "#{argv}/" : argv
+  end
+  base_path || ''
+end
+
+def return_large_value(value1, value2)
+  value1 > value2 ? value1 : value2
+end
+
+def device?(file_attr_type)
+  %w[b c].include?(file_attr_type)
+end
+
+# 文字列幅調整が必要な項目ごとの文字列長リスト
+def max_length_list(file_attr, max_length_list)
+  max_length_list[:hard_link] = return_large_value(max_length_list[:hard_link], file_attr[:hard_link].length)
+  max_length_list[:user] = return_large_value(max_length_list[:user], file_attr[:user].length)
+  max_length_list[:group] = return_large_value(max_length_list[:group], file_attr[:group].length)
+  if device?(file_attr[:type])
+    max_length_list[:major] = return_large_value(max_length_list[:major], file_attr[:major].length)
+    max_length_list[:minor] = return_large_value(max_length_list[:minor], file_attr[:minor].length)
+  else
+    max_length_list[:size] = return_large_value(max_length_list[:size], file_attr[:size].length)
+  end
+
+  # メジャー/マイナーの場合は " ," で連結されるためオフセットを含めて判定する
+  max_length_list[:size_or_version] = [max_length_list[:size], max_length_list[:major] + max_length_list[:minor] + DEVICE_DISPLAY_OFFSET].max
+end
+
+def file_attribute_hash(file, file_stat, full_path, max_length_list)
+  file_attr = {}
+  file_attr[:type] = convert_file_type(file_stat.ftype)
+  file_attr[:permission] = convert_permission(file_stat.mode)
+  file_attr[:hard_link] = file_stat.nlink.to_s
+  file_attr[:user] = Etc.getpwuid(file_stat.uid).name
+  file_attr[:group] = Etc.getpwuid(file_stat.gid).name
+  if device?(file_attr[:type])
+    file_attr[:major] = file_stat.rdev_major.to_s
+    file_attr[:minor] = file_stat.rdev_minor.to_s
+  else
+    file_attr[:size] = file_stat.size.to_s
+  end
+  mtime = file_stat.mtime
+  file_attr[:mtime] = format('%2<month>d月 %2<day>d %02<hour>d:%02<min>d', month: mtime.month, day: mtime.day, hour: mtime.hour, min: mtime.min)
+  file_attr[:name] = file_stat.symlink? ? "#{file} -> #{File.readlink(full_path)}" : file
+
+  max_length_list(file_attr, max_length_list)
+  file_attr
+end
+
+def format_list_style(file_attr_list, max_length_list)
+  file_attr_list.map do |file|
+    format_file_name = +"#{file[:type]}#{file[:permission]}"
+    format_file_name << " #{file[:hard_link].rjust(max_length_list[:hard_link])}"
+    format_file_name << " #{file[:user].ljust(max_length_list[:user])} #{file[:group].ljust(max_length_list[:group])}"
+    format_file_name << if device?(file[:type])
+                          " #{file[:major].rjust(max_length_list[:major])}, #{file[:minor].rjust(max_length_list[:minor])}"
+                        else
+                          " #{file[:size].rjust(max_length_list[:size_or_version])}"
+                        end
+    format_file_name << " #{file[:mtime]}"
+    format_file_name << " #{file[:name]}"
+  end
+end
+
+def print_list_style(argv, files)
+  # 最大文字列長リスト
+  max_length_list = {
+    hard_link: 0,
+    user: 0,
+    group: 0,
+    size: 0,
+    major: 0,
+    minor: 0,
+    size_or_version: 0
+  }
+
+  total_blocks = 0
+  base_path = base_path(argv)
+  file_attr_list = files.map do |file|
+    full_path = "#{base_path}#{file}"
+    file_stat = File.lstat(full_path)
+    total_blocks += file_stat.blocks
+    file_attribute_hash(file, file_stat, full_path, max_length_list)
+  end
+
+  # stat と ls の扱うブロック数が異なるため補正
+  puts "合計 #{total_blocks / 2}"
+  puts format_list_style(file_attr_list, max_length_list)
+end
+
+def print_default_style(files)
+  row_count = files.length / NUMBER_OF_COLUMNS
+  max_row = (files.length % NUMBER_OF_COLUMNS).zero? ? row_count : row_count + 1
+  file_names_list = files.each_slice(max_row).to_a
+
+  file_names_hash = file_names_list.map do |file_names|
+    max_length = max_file_name_length(file_names)
+    file_names.map do |name|
+      length = display_length(name)
+      { name:, length:, max_length: }
+    end
+  end
+
+  max_row.times do |row|
+    file_names_hash.length.times do |col|
+      print_format_file_name(file_names_hash[col][row]) if file_names_hash[col][row]
+    end
+    puts
+  end
 end
 
 def main
@@ -112,112 +229,10 @@ def main
 
   return if files.empty?
 
-  # [-l] オプションが存在する場合は詳細情報付きで縦に表示する
   if params[:l]
-    base_path = ''
-    if !argv.empty? && FileTest.directory?(argv)
-      base_path = +argv
-      base_path << '/' if base_path[-1] != '/'
-    end
-
-    total_blocks = 0
-
-    # 項目別の最大文字列長
-    max_length_list = {
-      hard_link: 0,
-      user: 0,
-      group: 0,
-      size: 0,
-      major: 0,
-      minor: 0,
-      size_or_version: 0
-    }
-
-    file_attr_list = files.map do |file|
-      full_path = "#{base_path}#{file}"
-      file_stat = File.lstat(full_path)
-      total_blocks += file_stat.blocks
-
-      file_attr = {}
-
-      # ファイルのタイプ
-      file_attr[:type] = convert_file_type(file_stat.ftype)
-
-      # ファイルのパーミッション
-      file_attr[:permission] = convert_permission(file_stat.mode)
-
-      # ファイルのハードリンク
-      file_attr[:hard_link] = file_stat.nlink.to_s
-      max_length_list[:hard_link] = max_length_list[:hard_link] > file_attr[:hard_link].length ? max_length_list[:hard_link] : file_attr[:hard_link].length
-
-      # ファイルの所有者/グループ
-      user_name = Etc.getpwuid(file_stat.uid).name
-      file_attr[:user] = user_name
-      max_length_list[:user] = max_length_list[:user] > file_attr[:user].length ? max_length_list[:user] : file_attr[:user].length
-
-      group_name = Etc.getpwuid(file_stat.gid).name
-      file_attr[:group] = group_name
-      max_length_list[:group] = max_length_list[:group] > file_attr[:group].length ? max_length_list[:group] : file_attr[:group].length
-
-      # ファイルサイズ or メジャー/マイナー番号
-      if %w[b c].include?(file_attr[:type])
-        file_attr[:major] = file_stat.rdev_major.to_s
-        file_attr[:minor] = file_stat.rdev_minor.to_s
-        max_length_list[:major] = max_length_list[:major] > file_attr[:major].length ? max_length_list[:major] : file_attr[:major].length
-        max_length_list[:minor] = max_length_list[:minor] > file_attr[:minor].length ? max_length_list[:minor] : file_attr[:minor].length
-      else
-        file_attr[:size] = file_stat.size.to_s
-        max_length_list[:size] = max_length_list[:size] > file_attr[:size].length ? max_length_list[:size] : file_attr[:size].length
-      end
-
-      # 更新日時
-      mtime = file_stat.mtime
-      file_attr[:mtime] = format('%2<month>d月 %2<day>d %02<hour>d:%02<min>d', month: mtime.month, day: mtime.day, hour: mtime.hour, min: mtime.min)
-
-      # ファイル名
-      file_attr[:name] = file_stat.symlink? ? "#{file} -> #{File.readlink(full_path)}" : file
-
-      file_attr
-    end
-
-    # メジャー/マイナー番号の場合は " ," で連結されるため +2 のオフセットを行う
-    max_length_list[:size_or_version] = [max_length_list[:size], max_length_list[:major] + max_length_list[:minor] + 2].max
-
-    # stat と ls の扱うブロック数が異なるため補正
-    puts "合計 #{total_blocks / 2}"
-
-    file_attr_list.each do |file|
-      format_file_name = +"#{file[:type]}#{file[:permission]}"
-      format_file_name << " #{file[:hard_link].rjust(max_length_list[:hard_link])}"
-      format_file_name << " #{file[:user].ljust(max_length_list[:user])} #{file[:group].ljust(max_length_list[:group])}"
-      format_file_name << if file[:size]
-                            " #{file[:size].rjust(max_length_list[:size_or_version])}"
-                          else
-                            " #{file[:major].rjust(max_length_list[:major])}, #{file[:minor].rjust(max_length_list[:minor])}"
-                          end
-      format_file_name << " #{file[:mtime]}"
-      format_file_name << " #{file[:name]}"
-      puts format_file_name
-    end
+    print_list_style(argv, files)
   else
-    row_count = files.length / NUMBER_OF_COLUMNS
-    max_row = (files.length % NUMBER_OF_COLUMNS).zero? ? row_count : row_count + 1
-    file_names_list = files.each_slice(max_row).to_a
-
-    file_names_hash = file_names_list.map do |file_names|
-      max_length = max_file_name_length(file_names)
-      file_names.map do |name|
-        length = display_length(name)
-        { name:, length:, max_length: }
-      end
-    end
-
-    max_row.times do |row|
-      file_names_hash.length.times do |col|
-        print_format_file_name(file_names_hash[col][row]) if file_names_hash[col][row]
-      end
-      puts
-    end
+    print_default_style(files)
   end
 end
 
