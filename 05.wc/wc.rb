5cc5790e3217b4bb00c919b146bd22c9265b46bf
nekonote <nekonote.civ@gmail.com>
2023-03-02 11:54:01 +0000

    標準入力を含めた各パターン別の出力処理を作成

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,6 +4,9 @@
 
 require 'optparse'
 
+# オプション未指定の場合は最大文字列長が固定
+NO_OPTION_MAX_LENGTH = 7
+
 def option_params
   opt = OptionParser.new
   params = {}
@@ -22,30 +25,61 @@ def get_words_count(text)
   text.split(/[ \n\t]/).count { |element| !element.empty? }
 end
 
-def in_params?(params)
+# オプションを指定されているか？
+def options_exists?(params)
   params[:l] || params[:w] || params[:c]
+end
+
+def all_counts_to_string(text)
+  [
+    get_return_count(text).to_s,
+    get_words_count(text).to_s,
+    text.bytesize.to_s
+  ]
+end
+
+def exists_counts_to_string(text, params)
+  [
+    params[:l] ? get_return_count(text).to_s : '',
+    params[:w] ? get_words_count(text).to_s : '',
+    params[:c] ? text.bytesize.to_s : ''
+  ].reject(&:empty?)
 end
 
 def main
   params = option_params
-  file_name = ARGV[0]
-  file_text = File.read(file_name)
 
-  if in_params?(params)
-    puts [
-      params[:l] ? get_return_count(file_text).to_s : '',
-      params[:w] ? get_words_count(file_text).to_s : '',
-      params[:c] ? file_text.bytesize.to_s : ''
-    ].reject(&:empty?).join(' ')
-  else # no option
-    results = [
-      get_return_count(file_text).to_s,
-      get_words_count(file_text).to_s,
-      file_text.bytesize.to_s
-    ]
-    max_length = results.map(&:length).max
-    results << file_name
-    puts results.map { |result| result.rjust(max_length) }.join(' ')
+  if File.pipe?($stdin)
+    read_line = $stdin.read
+    if options_exists?(params)
+      if params.length == 1
+        results = exists_counts_to_string(read_line, params).join(' ')
+        puts results
+      else
+        results = exists_counts_to_string(read_line, params)
+        puts results.map { |result| result.rjust(NO_OPTION_MAX_LENGTH) }.join(' ')
+      end
+    else
+      results = all_counts_to_string(read_line)
+      puts results.map { |result| result.rjust(NO_OPTION_MAX_LENGTH) }.join(' ')
+    end
+  else
+    file_name = ARGV[0]
+    file_text = File.read(file_name)
+    if options_exists?(params)
+      if params.length == 1
+        results = exists_counts_to_string(file_text, params).push(file_name).join(' ')
+        puts results
+      else
+        results = exists_counts_to_string(file_text, params)
+        max_length = results.map(&:length).max
+        puts results.map { |result| result.rjust(max_length) }.push(file_name).join(' ')
+      end
+    else
+      results = all_counts_to_string(file_text)
+      max_length = results.map(&:length).max
+      puts results.map { |result| result.rjust(max_length) }.push(file_name).join(' ')
+    end
   end
 end
 
