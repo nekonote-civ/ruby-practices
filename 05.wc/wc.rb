6c6631bed3cd5891f3c661474231cac4d6a95983
nekonote <nekonote.civ@gmail.com>
2023-03-15 12:54:38 +0000

    ファイル指定の場合の出力方法をシンプルにまとめ、分岐数を減らした

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -28,34 +28,50 @@ def print_type_pipe(params)
   puts join_counts(counts, length)
 end
 
+def print_type_file(params)
+  counts_array = ARGV.map do |file_name|
+    get_counts(params, File.read(file_name))
+  end
+
+  # オプション数1 かつ ファイル数1 のパターン以外の場合は文字列幅が設定される
+  length = 0
+  unless single_option?(params) && single_file?
+    total_counts = sum_counts(counts_array, params)
+    length = total_counts.map { |_key, value| value.to_s.length }.max
+  end
+
+  # 各カラムの値を出力
+  counts_array.each_with_index do |counts, idx|
+    puts "#{join_counts(counts, length)} #{ARGV[idx]}"
+  end
+
+  return if single_file?
+
+  # 合計値を出力
+  total_counts = sum_counts(counts_array, params)
+  puts "#{join_counts(total_counts, length)} 合計"
+end
+
 def get_counts(params, text)
-  params.empty? ? counts_all(text) : counts_some(text, params)
+  counts = {}
+  counts[:line] = get_return_count(text) if params[:l] || params.empty?
+  counts[:word] = get_words_count(text) if params[:w] || params.empty?
+  counts[:size] = text.bytesize if params[:c] || params.empty?
+  counts
 end
 
 def single_option?(params)
   params.length == 1
 end
 
+def single_file?
+  ARGV.length == 1
+end
+
 def join_counts(counts, length)
   counts.map do |_key, value|
     value.to_s.rjust(length)
   end.join(' ')
-end
-
-def counts_all(text)
-  {
-    line: get_return_count(text),
-    word: get_words_count(text),
-    size: text.bytesize
-  }
-end
-
-def counts_some(text, params)
-  {
-    line: params[:l] ? get_return_count(text) : 0,
-    word: params[:w] ? get_words_count(text) : 0,
-    size: params[:c] ? text.bytesize : 0
-  }.reject { |_key, value| value.zero? }
 end
 
 def get_return_count(text)
@@ -66,65 +82,22 @@ def get_words_count(text)
   text.split(/[ \n\t]/).count { |t| !t.empty? }
 end
 
-# 縦列毎の合計値の配列を作成
-def total_counts(counts)
-  Array.new(counts[0].length) { |col| Array.new(counts.length) { |row| counts[row][col] }.sum }
-end
-
-# [[a, b, c], [x, y, z]] のような2次元配列から表示文字列幅を算出
-# 下記の様に オプションが1つ かつ 指定ファイルが1つ 以外の場合はここでカラム毎の幅が決定される
-# 例) wc.rb -l test.txt
-def column_print_length
-  counts = ARGV.map do |file_name|
-    file_text = File.read(file_name)
-    counts_all(file_text)
-  end
-  counts << total_counts(counts)
-  counts.map { |count| count.map { |count_item| count_item.to_s.length }.max }.max
-end
-
-# 0始まりの配列の最後に到達したか？
-def last_array_index?(idx, array_length)
-  idx == array_length - 1
-end
-
-def format_join_counts(is_last, join_counts, file_name)
-  is_last ? "#{join_counts} 合計" : "#{join_counts} #{file_name}"
-end
-
-def print_single_params(counts, file_names)
-  counts = counts.flatten(1)
-  if file_names.length == 1
-    puts "#{counts[0]} #{file_names[0]}"
-  else
-    counts << counts.sum
-    length = column_print_length
-    counts.each_with_index do |count, idx|
-      join_counts = count.to_s.rjust(length)
-      puts format_join_counts(last_array_index?(idx, counts.length), join_counts, file_names[idx])
+def sum_counts(counts_array, params)
+  total_counts = init_total_counts(params)
+  counts_array.each do |counts|
+    counts.each do |key, value|
+      total_counts[key] += value if total_counts[key]
     end
   end
+  total_counts
 end
 
-def print_multi_params(counts, file_names)
-  length = column_print_length
-  if file_names.length == 1
-    counts.each_with_index do |count, idx|
-      join_counts = join_counts(count, length)
-      puts "#{join_counts} #{file_names[idx]}"
-    end
-  else
-    counts << total_counts(counts)
-    counts.each_with_index do |count, idx|
-      join_counts = join_counts(count, length)
-      puts format_join_counts(last_array_index?(idx, counts.length), join_counts, file_names[idx])
-    end
-  end
-end
-
-def print_type_file(params)
-  counts = ARGV.map { |file_name| get_counts(params, File.read(file_name)) }
-  params.length == 1 ? print_single_params(counts, ARGV) : print_multi_params(counts, ARGV)
+def init_total_counts(params)
+  total_counts = {}
+  total_counts[:line] = 0 if params[:l] || params.empty?
+  total_counts[:word] = 0 if params[:w] || params.empty?
+  total_counts[:size] = 0 if params[:c] || params.empty?
+  total_counts
 end
 
 # メイン処理実行
